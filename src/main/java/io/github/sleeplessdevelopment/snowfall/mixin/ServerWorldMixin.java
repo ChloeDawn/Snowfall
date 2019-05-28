5be577d42ee8e94133d95739e546afc92be57b13
Chloe Dawn <admin@designdevils.co.uk>
2019-05-28 13:19:40 +0100

    Replace redirection with argument modification

--- src/main/java/io/github/sleeplessdevelopment/snowfall/mixin/ServerWorldMixin.java
+++ src/main/java/io/github/sleeplessdevelopment/snowfall/mixin/ServerWorldMixin.java
@@ -6,29 +6,32 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.SnowBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(ServerWorld.class)
-abstract class ServerWorldMixin {
+abstract class ServerWorldMixin extends World {
   private ServerWorldMixin() {
+    super(null, DimensionType.OVERWORLD, (lvl, dim) -> null, null, false);
     throw new AssertionError();
   }
 
-  @Redirect(
+  @ModifyArg(
     method = "tickChunk",
     at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z"),
     slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/world/biome/Biome;canSetIce(Lnet/minecraft/world/ViewableWorld;Lnet/minecraft/util/math/BlockPos;)Z")))
-  private boolean snowfall$stackSnowLayers(final ServerWorld world, final BlockPos pos, final BlockState state) {
-    final BlockState existing = world.getBlockState(pos);
+  private BlockState snowfall$stackSnowLayers(final BlockPos pos, final BlockState state) {
+    final BlockState existing = this.getBlockState(pos);
     if (Blocks.SNOW == existing.getBlock()) {
       final int nextLayer = existing.get(SnowBlock.LAYERS) + 1;
       if (SnowLayers.contains(nextLayer)) {
-        return world.setBlockState(pos, existing.with(SnowBlock.LAYERS, nextLayer));
+        return existing.with(SnowBlock.LAYERS, nextLayer);
       }
     }
-    return world.setBlockState(pos, state);
+    return state;
   }
 }
